1ac6ed33d495857366bf253192da37646e2ea4f7
Maurox28363429 <maur28363429rd@gmail.com>
2024-01-23 17:35:48 -0400

    change

---
+++ lib/webview.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(DashboardApp());
+
+class DashboardApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dashboard App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: DashboardScreen(),
+    );
+  }
+}
+
+class DashboardScreen extends StatefulWidget {
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedTabIndex = 0;
+  File? _image;
+
+  void _onTabSelected(int index) {
+    setState(() {
+      _selectedTabIndex = index;
+    });
+  }
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.getImage(source: ImageSource.camera);
+
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Dashboard App',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Inicio'),
+              selected: _selectedTabIndex == 0,
+              onTap: () => _onTabSelected(0),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Perfil'),
+              selected: _selectedTabIndex == 1,
+              onTap: () => _onTabSelected(1),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Configuración'),
+              selected: _selectedTabIndex == 2,
+              onTap: () => _onTabSelected(2),
+            ),
+          ],
+        ),
+      ),
+      body: IndexedStack(
+        index: _selectedTabIndex,
+        children: <Widget>[
+          // Contenido de la página de inicio
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Página de Inicio'),
+                SizedBox(height: 20),
+                _image != null
+                    ? Image.file(
+                  _image!,
+                  height: 200,
+                )
+                    : Container(),
+              ],
+            ),
+          ),
+          // Contenido de la página de perfil
+          Center(
+            child: Text('Página de Perfil'),
+          ),
+          // Contenido de la página de configuración
+          Center(
+            child: WebView(
+              initialUrl: 'https://www.google.com',
+              javascriptMode: JavascriptMode.unrestricted,
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTabIndex,
+        onTap: _onTabSelected,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Configuración',
+          ),
+        ],
+      ),
+      floatingActionButton: _selectedTabIndex == 0
+          ? FloatingActionButton(
+        onPressed: _takePicture,
+        tooltip: 'Tomar foto',
+        child: Icon(Icons.camera),
+      )
+          : null,
+    );
+  }
+}
